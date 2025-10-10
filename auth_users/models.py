ab078469ed7f6388743b78a68eefd2985a0a7e04
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-10-11 00:30:16 +0600

    Custom user and custom superuser created successfully and.

--- auth_users/models.py
+++ auth_users/models.py
@@ -17,30 +17,37 @@ class CustomUserManager(BaseUserManager):
         user.set_password(password)
         user.save(using=self._db)
         
-    def create_superuser(self, username, email, password, **extra_fileds):
+        return user
+        
+    def create_superuser(self, email, password, **extra_fileds):
         """
             Create a superuser with the email , username and password 
             who can access the admin panel
         """
         
-        user = self.create_user( username,email, password = password)
+        user = self.create_user(email, password = password)
+        user.is_staff = True
         user.is_admin = True
+        user.is_superuser = True
         user.save(using=self._db)
         
         return user
     
     
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     
     username = models.CharField(max_length=200)
     email = models.EmailField(verbose_name="email address", max_length=100, unique=True)
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    # data_joined = models.DateTimeField(auto_now_add=True)
     
     objects = CustomUserManager()
     
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = "username"
+    REQUIRED_FIELDS = ["username"]
     
     def __str__(self):
         return f"{self.email}"
