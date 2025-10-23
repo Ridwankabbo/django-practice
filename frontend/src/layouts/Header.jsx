6f56871b35aa36acc9c919e67019fe8b2b33f79b
Ridwankabbo <ridwanislamkabbo@gmail.com>
2025-10-23 15:44:15 +0600

    admin page redirect to django default admin page

--- frontend/src/layouts/Header.jsx
+++ frontend/src/layouts/Header.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 export default function Header(){
+    // const navigate = useNavigate();
+    const DJANGO_ADMIN_URL = 'http://127.0.0.1:8000/admin/';
+
+    const handleRedirectAction = async()=>{
+        // const request = await fetch(DJANGO_ADMIN_URL);
+        window.location.href=DJANGO_ADMIN_URL
+        
+    }
     return(
         <>
             <section className="flex justify-around items-center bg-green-700 text-white py-5">
@@ -12,6 +19,7 @@ export default function Header(){
                     <Link to={'/'} className="py-3">Home</Link>
                     <Link to={'/registration'} className="bg-blue-700 text-white px-7 py-3 rounded-xl ">registratoin</Link>
                     <Link to={'/login'} className="bg-yellow-500 text-white px-7 py-3 rounded-xl">Login</Link>
+                    <Link t0={'/admin'} className="bg-orange-500 text-white px-7 py-3 rounded-xl" onClick={handleRedirectAction}>Admin</Link>
                 </div>
             </section>
         </>
